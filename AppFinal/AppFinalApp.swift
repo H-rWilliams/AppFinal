232dcd1543a9553625ceab2eec2800ab69f1140a
Hunter Williams <hw0923309@otc.edu>
2025-04-30 08:28:41 -0500

    Initial Commit

---
+++ AppFinal/AppFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppFinalApp.swift
+//  AppFinal
+//
+//  Created by WILLIAMS, HUNTER R. on 4/30/25.
+//
+
+import SwiftUI
+
+@main
+struct AppFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppFinalTests/AppFinalTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppFinalTests.swift
+//  AppFinalTests
+//
+//  Created by WILLIAMS, HUNTER R. on 4/30/25.
+//
+
+import Testing
+@testable import AppFinal
+
+struct AppFinalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
